87872850e957d5e9413a965e5552c0ce5489129f
Agnij Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2025-03-30 14:14:23 +0530

    modified:   .gitattributes 	modified:   .gitignore 	modified:   tests/load_boston.py

--- tests/load_boston.py
+++ tests/load_boston.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def get_data():
-    data = pd.read_csv("BostonHousing.csv")
+    data = pd.read_csv(r"/home/snape/projects/Mojmelo/tests/BostonHousing.csv")
 
     return [data.iloc[:, :-1].values, data.iloc[:, -1].values.reshape(-1,1)]
